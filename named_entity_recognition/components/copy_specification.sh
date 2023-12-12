456d58f7625d2bc5c9a9e310a79201ccb65f6624
ght163988 <ght163988@autonavi.com>
2023-12-12 17:45:49 +0800

    oss

--- named_entity_recognition/components/copy_specification.sh
+++ named_entity_recognition/components/copy_specification.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
-BUCKET="your-bucket-name"
+BUCKET="amap-meta-3d"
 
 echo "\nCopy component specifications to Google Cloud Storage"
-gsutil cp preprocess/component.yaml gs://${BUCKET}/components/preprocess/component.yaml
-gsutil acl ch -u AllUsers:R gs://${BUCKET}/components/preprocess/component.yaml
 
-gsutil cp train/component.yaml gs://${BUCKET}/components/train/component.yaml
-gsutil acl ch -u AllUsers:R gs://${BUCKET}/components/train/component.yaml
 
-gsutil cp deploy/component.yaml gs://${BUCKET}/components/deploy/component.yaml
-gsutil acl ch -u AllUsers:R gs://${BUCKET}/components/deploy/component.yaml
+ossutil cp preprocess/component.yaml oss://${BUCKET}/kubeflow/components/preprocess/component.yaml -i LTAI5tKhWZQjyGDX2FvMdcPu -k ZbHFmxWADeXgToIEykbhyFQVicnmsW
+ossutil acl ch -u AllUsers:R gs://${BUCKET}/components/preprocess/component.yaml -i LTAI5tKhWZQjyGDX2FvMdcPu -k ZbHFmxWADeXgToIEykbhyFQVicnmsW
+
+ossutil cp train/component.yaml oss://${BUCKET}/kubeflow/components/train/component.yaml -i LTAI5tKhWZQjyGDX2FvMdcPu -k ZbHFmxWADeXgToIEykbhyFQVicnmsW
+ossutil acl ch -u AllUsers:R gs://${BUCKET}/components/train/component.yaml -i LTAI5tKhWZQjyGDX2FvMdcPu -k ZbHFmxWADeXgToIEykbhyFQVicnmsW
+
+ossutil cp deploy/component.yaml oss://${BUCKET}/kubeflow/components/deploy/component.yaml -i LTAI5tKhWZQjyGDX2FvMdcPu -k ZbHFmxWADeXgToIEykbhyFQVicnmsW
+ossutil acl ch -u AllUsers:R gs://${BUCKET}/components/deploy/component.yaml -i LTAI5tKhWZQjyGDX2FvMdcPu -k ZbHFmxWADeXgToIEykbhyFQVicnmsW
