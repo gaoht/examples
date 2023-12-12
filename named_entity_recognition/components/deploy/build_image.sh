5fbb05c4dfd0233a2bd8dbb71f29bb6083509e8e
ght163988 <ght163988@autonavi.com>
2023-12-12 18:12:56 +0800

    sec

--- named_entity_recognition/components/deploy/build_image.sh
+++ named_entity_recognition/components/deploy/build_image.sh
@@ -8,6 +8,5 @@ full_image_name=${image_name}:${image_tag}
 cd "$(dirname "$0")"
 
 
-docker login -u gaoht1987@gmail.com -p '1q2w3e4r?' registry.cn-zhangjiakou.aliyuncs.com
 docker build -t "${full_image_name}" .
 docker push "$full_image_name"
--- named_entity_recognition/components/train/build_image.sh
+++ named_entity_recognition/components/train/build_image.sh
@@ -9,6 +9,5 @@ base_image_tag=1.12.0-py3
 
 cd "$(dirname "$0")"
 
-docker login -u gaoht1987@gmail.com -p '1q2w3e4r?' registry.cn-zhangjiakou.aliyuncs.com
 docker build --build-arg BASE_IMAGE_TAG=${base_image_tag} -t "${full_image_name}" .
 docker push "$full_image_name"
