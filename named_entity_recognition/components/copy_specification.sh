f89a78141b7ed2de1c246fffeacd7f306cab268c
ght163988 <ght163988@autonavi.com>
2023-12-12 17:47:15 +0800

    oss

--- named_entity_recognition/components/copy_specification.sh
+++ named_entity_recognition/components/copy_specification.sh
@@ -5,11 +5,11 @@ BUCKET="amap-meta-3d"
 echo "\nCopy component specifications to Google Cloud Storage"
 
 
-ossutil cp preprocess/component.yaml oss://${BUCKET}/kubeflow/components/preprocess/component.yaml -i LTAI5tKhWZQjyGDX2FvMdcPu -k ZbHFmxWADeXgToIEykbhyFQVicnmsW
-ossutil acl ch -u AllUsers:R gs://${BUCKET}/components/preprocess/component.yaml -i LTAI5tKhWZQjyGDX2FvMdcPu -k ZbHFmxWADeXgToIEykbhyFQVicnmsW
+ossutil cp preprocess/component.yaml oss://${BUCKET}/kubeflow/components/preprocess/component.yaml -e oss-cn-zhangjiakou.aliyuncs.com -i LTAI5tKhWZQjyGDX2FvMdcPu -k ZbHFmxWADeXgToIEykbhyFQVicnmsW
+ossutil acl ch -u AllUsers:R gs://${BUCKET}/components/preprocess/component.yaml -e oss-cn-zhangjiakou.aliyuncs.com -i LTAI5tKhWZQjyGDX2FvMdcPu -k ZbHFmxWADeXgToIEykbhyFQVicnmsW
 
-ossutil cp train/component.yaml oss://${BUCKET}/kubeflow/components/train/component.yaml -i LTAI5tKhWZQjyGDX2FvMdcPu -k ZbHFmxWADeXgToIEykbhyFQVicnmsW
-ossutil acl ch -u AllUsers:R gs://${BUCKET}/components/train/component.yaml -i LTAI5tKhWZQjyGDX2FvMdcPu -k ZbHFmxWADeXgToIEykbhyFQVicnmsW
+ossutil cp train/component.yaml oss://${BUCKET}/kubeflow/components/train/component.yaml -e oss-cn-zhangjiakou.aliyuncs.com -i LTAI5tKhWZQjyGDX2FvMdcPu -k ZbHFmxWADeXgToIEykbhyFQVicnmsW
+ossutil acl ch -u AllUsers:R gs://${BUCKET}/components/train/component.yaml -e oss-cn-zhangjiakou.aliyuncs.com -i LTAI5tKhWZQjyGDX2FvMdcPu -k ZbHFmxWADeXgToIEykbhyFQVicnmsW
 
-ossutil cp deploy/component.yaml oss://${BUCKET}/kubeflow/components/deploy/component.yaml -i LTAI5tKhWZQjyGDX2FvMdcPu -k ZbHFmxWADeXgToIEykbhyFQVicnmsW
-ossutil acl ch -u AllUsers:R gs://${BUCKET}/components/deploy/component.yaml -i LTAI5tKhWZQjyGDX2FvMdcPu -k ZbHFmxWADeXgToIEykbhyFQVicnmsW
+ossutil cp deploy/component.yaml oss://${BUCKET}/kubeflow/components/deploy/component.yaml -e oss-cn-zhangjiakou.aliyuncs.com -i LTAI5tKhWZQjyGDX2FvMdcPu -k ZbHFmxWADeXgToIEykbhyFQVicnmsW
+ossutil acl ch -u AllUsers:R gs://${BUCKET}/components/deploy/component.yaml -e oss-cn-zhangjiakou.aliyuncs.com -i LTAI5tKhWZQjyGDX2FvMdcPu -k ZbHFmxWADeXgToIEykbhyFQVicnmsW
