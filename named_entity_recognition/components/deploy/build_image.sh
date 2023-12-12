106e7c0f52e0842e8801a749ebf0d8c0daa55db8
ght163988 <ght163988@autonavi.com>
2023-12-12 17:10:17 +0800

    docker file

--- named_entity_recognition/components/deploy/build_image.sh
+++ named_entity_recognition/components/deploy/build_image.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
-image_name=gcr.io/$PROJECT_ID/kubeflow/ner/deploy
+image_name=registry.cn-zhangjiakou.aliyuncs.com/iamght/kubeflow/ner/deploy
 image_tag=latest
 
 full_image_name=${image_name}:${image_tag}
 
-cd "$(dirname "$0")" 
+cd "$(dirname "$0")"
 
+
+docker login -u gaoht1987@gmail.com -p '1q2w3e4r?' registry.cn-zhangjiakou.aliyuncs.com
 docker build -t "${full_image_name}" .
 docker push "$full_image_name"
--- named_entity_recognition/components/train/build_image.sh
+++ named_entity_recognition/components/train/build_image.sh
@@ -1,13 +1,14 @@
 
 #!/bin/sh
 
-image_name=gcr.io/$PROJECT_ID/kubeflow/ner/train
+image_name=registry.cn-zhangjiakou.aliyuncs.com/iamght/kubeflow/ner/train
 image_tag=latest
 
 full_image_name=${image_name}:${image_tag}
 base_image_tag=1.12.0-py3
 
-cd "$(dirname "$0")" 
+cd "$(dirname "$0")"
 
+docker login -u gaoht1987@gmail.com -p '1q2w3e4r?' registry.cn-zhangjiakou.aliyuncs.com
 docker build --build-arg BASE_IMAGE_TAG=${base_image_tag} -t "${full_image_name}" .
 docker push "$full_image_name"
